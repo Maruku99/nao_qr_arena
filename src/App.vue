ba511fb73df4ac9e92fcd24f78bedc344ab1ccee
marc wendt <mail@marcwendt.com>
2026-04-23 17:58:50 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <HeaderArea/>
+      <SizeFields/>
+      <v-btn
+        class="m-2"
+        icon="mdi-theme-light-dark"
+        location="top right"
+        position="absolute"
+        @click="$vuetify.theme.cycle()"
+      />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import HeaderArea from "@/components/HeaderArea.vue";
+import SizeFields from "./components/SizeFields.vue";
+</script>
---
+++ src/components/SizeFields.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-responsive class="m-4">
+    <v-row>
+        <v-number-input
+        :reverse="false"
+        controlVariant="split"
+        label="Länge Arena"
+        :hideInput="false"
+        :inset="false"
+        ></v-number-input>
+        <v-number-input
+        :reverse="false"
+        controlVariant="split"
+        label="Breite Arena"
+        :hideInput="false"
+        :inset="false"
+        ></v-number-input>
+    </v-row>
+  </v-responsive>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const labelText = ref()
+</script>
