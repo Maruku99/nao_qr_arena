a42579ce1e68688bea92d664d850261a0ec570e5
marc wendt <mail@marcwendt.com>
2026-04-23 19:04:53 +0200

    Formatierung des SizeFields.vue für bessere Lesbarkeit

--- src/components/SizeFields.vue
+++ src/components/SizeFields.vue
@@ -1,23 +1,23 @@
 <template>
   <v-responsive>
-        <v-number-input
-        :reverse="false"
-        controlVariant="split"
-        :label="props.label"
-        :hideInput="false"
-        :inset="false"
-        :min="1"
-        ></v-number-input>
+    <v-number-input
+      :reverse="false"
+      controlVariant="split"
+      :label="props.label"
+      :hideInput="false"
+      :inset="false"
+      :min="1"
+    ></v-number-input>
   </v-responsive>
 </template>
 
 <script setup lang="ts">
 const props = withDefaults(
   defineProps<{
-    label: string
+    label: string;
   }>(),
   {
-    label: "label"
-  }
-)
+    label: "label",
+  },
+);
 </script>
