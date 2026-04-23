7935367801a330148658447882f01633ace98266
marc wendt <mail@marcwendt.com>
2026-04-23 19:37:43 +0200

    Ändere die Hintergrundfarbe des Karten-Elements in GridField.vue für nicht ausgewählte Zustände

--- src/components/GridField.vue
+++ src/components/GridField.vue
@@ -2,7 +2,7 @@
   <v-item-group multiple>
     <v-item v-slot="{ isSelected, toggle }">
       <v-card
-        :color="isSelected ? 'bg-red-300' : 'bg-blue-300'"
+        :color="isSelected ? 'bg-red-300' : ''"
         height="50"
         width="50"
         @click="toggle"
