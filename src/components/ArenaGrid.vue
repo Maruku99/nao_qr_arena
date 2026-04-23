cbe581995c4b446b1f171795b831de6c9f788a24
marc wendt <mail@marcwendt.com>
2026-04-23 19:33:46 +0200

    Erstelle ArenaGrid.vue mit GridField-Komponenten

---
+++ src/components/ArenaGrid.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="grid grid-cols-3 gap-2 w-fit m-4">
+    <GridField />
+    <GridField />
+    <GridField />
+    <GridField />
+    <GridField />
+    <GridField />
+    <GridField />
+    <GridField />
+    <GridField />
+  </div>
+</template>
+
+<script setup lang="ts">
+import GridField from "./GridField.vue";
+</script>
