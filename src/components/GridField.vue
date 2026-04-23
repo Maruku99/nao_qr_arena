fbc9d4a732bb3a53749f2712e64715f945674b80
marc wendt <mail@marcwendt.com>
2026-04-23 19:04:44 +0200

    anfang

---
+++ src/components/GridField.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-item-group multiple>
+    <v-container>
+      <v-row class="m-auto">
+        <v-col v-for="n in 24" :key="n">
+          <v-item v-slot="{ isSelected, toggle }">
+            <v-card
+              :color="isSelected ? 'bg-red-300' : 'bg-blue-300'"
+              height="50"
+              width="50"
+              @click="toggle"
+            >
+            </v-card>
+          </v-item>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-item-group>
+</template>
+
+<script>
+
+</script>
