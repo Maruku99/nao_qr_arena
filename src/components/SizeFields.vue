76e08beb8c2b33ecd97b8654edd91bf99a2e3a23
marc wendt <mail@marcwendt.com>
2026-04-25 01:32:11 +0200

    Füge v-model und Watcher in SizeFields.vue hinzu: Ermögliche die Aktualisierung des Eingabewerts

--- src/components/SizeFields.vue
+++ src/components/SizeFields.vue
@@ -1,17 +1,28 @@
 <template>
   <v-responsive>
     <v-number-input
+      v-model="input"
       :reverse="false"
       controlVariant="split"
       :label="props.label"
       :hideInput="false"
       :inset="false"
       :min="1"
-    ></v-number-input>
-  </v-responsive>
+      ></v-number-input>
+    </v-responsive>
 </template>
 
 <script setup lang="ts">
+import { ref, watch} from 'vue';
+
+const emit = defineEmits<{ (e: 'update:modelValue', value: number): void }>()
+
+const input = ref(1)
+
+watch(input, (newValue) => {
+  emit('update:modelValue', newValue)
+})
+
 const props = withDefaults(
   defineProps<{
     label: string;
