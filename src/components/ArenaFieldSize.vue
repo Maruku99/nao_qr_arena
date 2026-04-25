3ea8a03511b631cab98704b4176e762a97788788
marc wendt <mail@marcwendt.com>
2026-04-25 10:25:31 +0200

    Füge ArenaFieldSize-Komponente hinzu: Implementiere Eingabefelder für Feldlängen und -breiten

---
+++ src/components/ArenaFieldSize.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-responsive>
+    <v-row class="gap-2 pr-4 pl-4 mt-4">
+      <v-col>
+        <v-text-field
+          v-model="fieldLength"
+          hide-details="auto"
+          label="Länge eines Feldes in cm"
+          clearable
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+          v-model="fieldWidth"
+          hide-details="auto"
+          label="Breite eines Feldes in cm"
+          clearable
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-responsive>
+</template>
+
+<script setup lang="ts">
+import { useArenaStore } from "@/stores/useArenaStore";
+
+const { fieldLength, fieldWidth } = useArenaStore();
+
+</script>
